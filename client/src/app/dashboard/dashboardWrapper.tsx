d7c97c874cef00243550ab1ebaf5dfc4e29e0592
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-08-02 09:28:41 +0545

    redux setup

---
+++ client/src/app/dashboard/dashboardWrapper.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Navbar from "../(components)/Navbar";
+import Sidebar from "../(components)/Sidebar";
+import StoreProvider from "../redux";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="light flex bg-gray-50 text-gray-900 w-full min-h-screen">
+      <Sidebar />
+      <main className="flex flex-col w-full h-full py-9 px-9 bg-gray-50 md:pl-24">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+}
+
+function DashboardWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+}
+
+export default DashboardWrapper;
