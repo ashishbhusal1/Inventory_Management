b68ce5357af4d39ed4611c41c4287e3782df9f26
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-08-04 21:55:37 +0545

    controllers

---
+++ server/src/controllers/dasboardController.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+
+const prisma = new PrismaClient();
+
+
+export const getDashboardMetrics = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const popularProducts = await prisma.products.findMany({
+            take: 15,
+            orderBy: {
+                stockQuantity: "desc"
+            }
+        });
+        const salesSummary = await prisma.salesSummary.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            }
+        });
+        const purchaseSummary = await prisma.purchaseSummary.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            }
+        });
+        const expenseSummary = await prisma.expenseSummary.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            }
+        });
+        const expenseByCategoryRaw = await prisma.expenseByCategory.findMany({
+            take: 5,
+            orderBy: {
+                date: "desc"
+            }
+        });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error retrieving dashboard metrics" });
+    }
+}
