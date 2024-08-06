c724710ab87e997e6bebbe2602f8721025543a2d
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-08-06 19:02:05 +0545

    users page

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import dashboardRoutes from './routes/dashboardRoutes';
 import productRoutes from './routes/productRoutes';
+import userRoutes from './routes/userRoutes';
 // Route imports
 
 // Configurations
@@ -22,6 +23,7 @@ app.use(cors());
 // Routes
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 // Server
 const port = process.env.PORT || 3001;
